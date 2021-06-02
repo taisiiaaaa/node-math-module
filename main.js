53d060b4b8079cee90a46489572eafd20d417d6f
taisiiaaaa <taisiya.p@ukr.net>
2021-06-02 21:44:32 +0300

    add math module

---
+++ main.js
@@ -0,0 +1,5 @@
+const { min, abs, summ } = require('math')
+
+console.log(`min(): ${min([4, 2, 10, 7, -19, 0, -5, 9])}`)
+console.log(`abs(): ${abs(-10)}`)
+console.log(`summ(): ${summ(10, 3, 14, 35, 100)}`)
